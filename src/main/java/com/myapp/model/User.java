4393d64e06c232fd2d7bd606676749652224c258
poll_finnegan <jj221210@gmail.com>
2018-08-14 14:51:58 +0300

    Initial commit

---
+++ src/main/java/com/myapp/model/User.java
@@ -0,0 +1,35 @@
+package com.myapp.model;
+
+
+public class User {
+
+    private long userId;
+    private String login;
+    private String password;
+
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
